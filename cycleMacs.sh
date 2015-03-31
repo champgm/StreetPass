5a9396c730393c0d79f45922774c2617dcf08b29
Riley Mills <rileypmills@gmail.com>
2015-03-30 23:27:38 -0500

    Added a check to require running with sudo. Added a check to determine if the user is on Yosemite or later so we can specify the correct Internet Connection Sharing plist.

--- cycleMacs.sh
+++ cycleMacs.sh
@@ -4,8 +4,23 @@ set -e
 #Un-comment this next line if you want to see exactly what this script is doing
 #set -x
 
+if [ "$(whoami)" != "root" ]
+then
+  	echo "You must run this script with sudo."
+  	exit 1
+fi
+
 #Assign a default value if the network adapter name is missing
 adapter=${2-en0}
+
+osMinorVersion=$(sw_vers -productVersion | awk -F'.' '{ print $2 }')
+
+if [ $(echo "$osMinorVersion < 10" | bc) -eq 0 ]
+		then
+			icsPlist='/System/Library/LaunchDaemons/com.apple.NetworkSharing.plist'
+		else
+			icsPlist='/System/Library/LaunchDaemons/com.apple.InternetSharing.plist'
+fi
 
 #Check the number of arguments and confirm with user if they are correct
 if [ $# -lt 1 ]
@@ -16,7 +31,7 @@ if [ $# -lt 1 ]
 			echo "Setting mac address to $mac"	
 			ifconfig $adapter ether $mac
 			echo "Disabling ICS and sleeping for 10s"
-			sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.InternetSharing.plist
+			sudo launchctl unload -w $icsPlist
 			sleep 10
 			echo "Disabling adapter and sleeping for 10s"
 			networksetup -setairportpower $adapter off
@@ -25,7 +40,7 @@ if [ $# -lt 1 ]
 			networksetup -setairportpower $adapter on
 			sleep 10
 			echo "Enabling ICS and sleeping for 5 minutes"
-			sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.InternetSharing.plist
+			sudo launchctl load -w $icsPlist
 			sleep 300
 			done <$1
 fi
