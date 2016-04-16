05d47eb75b516ca5e55243a3c0cbfa29aec843fb
Mac <gilbertmccoy@gmail.com>
2016-04-16 11:44:05 -0500

    Changes to logic and added an AppleScript

--- cycleMacs.sh
+++ cycleMacs.sh
@@ -13,35 +13,41 @@ fi
 #Assign a default value if the network adapter name is missing
 adapter=${2-en0}
 
-osMinorVersion=$(sw_vers -productVersion | awk -F'.' '{ print $2 }')
-
-if [ $(echo "$osMinorVersion < 10" | bc) -eq 0 ]
-		then
-			icsPlist='/System/Library/LaunchDaemons/com.apple.NetworkSharing.plist'
-		else
-			icsPlist='/System/Library/LaunchDaemons/com.apple.InternetSharing.plist'
-fi
-
 #Check the number of arguments and confirm with user if they are correct
 if [ $# -lt 1 ]
         then echo "At least one argument must be specified: <REQUIRED: File containing MAC list> <OPTIONAL: Network adapter name (Default is 'en0')>"
                 exit 1
         else
 			while IFS=, read mac; do
-			echo "Setting mac address to $mac"	
-			ifconfig $adapter ether $mac
-			echo "Disabling ICS and sleeping for 10s"
-			sudo launchctl unload -w $icsPlist
-			sleep 10
-			echo "Disabling adapter and sleeping for 10s"
-			networksetup -setairportpower $adapter off
-			sleep 10
-			echo "Enabling adapter and sleeping for 10s"
-			networksetup -setairportpower $adapter on
-			sleep 10
+			echo "Setting mac address to ${mac}"
+			ifconfig ${adapter} ether ${mac}
+
 			echo "Enabling ICS and sleeping for 5 minutes"
-			sudo launchctl load -w $icsPlist
+			if [ ! -f /System/Library/LaunchDaemons/com.apple.InternetSharing.plist ]
+			    then
+			        osascript toggleSharing.scpt
+			    else
+			        sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.InternetSharing.plist
+			fi
 			sleep 300
+
+			echo "Disabling adapter and sleeping for 10s"
+			networksetup -setairportpower ${adapter} off
+			sleep 10
+
+			echo "Enabling adapter and sleeping for 10s"
+			networksetup -setairportpower ${adapter} on
+			sleep 10
+
+			echo "Disabling ICS and sleeping for 10s"
+			if [ ! -f /System/Library/LaunchDaemons/com.apple.InternetSharing.plist ]
+			    then
+			        osascript toggleSharing.scpt
+			    else
+			        sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.InternetSharing.plist
+			fi
+			sleep 10
+
 			done <$1
 fi
 
